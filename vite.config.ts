47c87a374ca36c2d57d53516e38821c30300da69
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-07-24 23:36:41 +1000

    fixing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pet-sitting-services-app",
+  base: "/pet-sitting-services-app/",
   plugins: [react()],
   build: {
     outDir: 'dist', 
